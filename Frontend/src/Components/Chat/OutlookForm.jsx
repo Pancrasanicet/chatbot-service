711305f7d718222f02a801df96e0c5029649686a
Pancras Anicet <pancrasanicet@gmail.com>
2024-09-24 19:53:39 +0100

    finalisation des forms

--- Frontend/src/Components/Chat/OutlookForm.jsx
+++ Frontend/src/Components/Chat/OutlookForm.jsx
@@ -8,57 +8,100 @@ function OutlookForm({ onSubmit }) {
   } = useForm();
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("firstName", { required: true })} placeholder="Nom" />
-      {errors.firstName && <span>Ce champ est requis</span>}
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="space-y-4 p-4 bg-white shadow-lg rounded-lg max-w-md "
+    >
+      <input
+        {...register("firstName", { required: true })}
+        placeholder="Nom"
+        className="w-full p-2 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500"
+      />
+      {errors.firstName && (
+        <span className="text-red-500 text-sm">Ce champ est requis</span>
+      )}
 
       <input
         {...register("lastName", { required: true })}
         placeholder="Prénoms"
+        className="w-full p-2 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500"
       />
-      {errors.lastName && <span>Ce champ est requis</span>}
+      {errors.lastName && (
+        <span className="text-red-500 text-sm">Ce champ est requis</span>
+      )}
 
-      <input {...register("email", { required: true })} placeholder="Email" />
-      {errors.email && <span>Ce champ est requis</span>}
+      <input
+        {...register("email", { required: true })}
+        placeholder="Email"
+        className="w-full p-2 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500"
+      />
+      {errors.email && (
+        <span className="text-red-500 text-sm">Ce champ est requis</span>
+      )}
 
       <input
         {...register("phone", { required: true })}
         placeholder="Numéro de téléphone"
+        className="w-full p-2 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500"
       />
-      {errors.phone && <span>Ce champ est requis</span>}
-
+      {errors.phone && (
+        <span className="text-red-500 text-sm">Ce champ est requis</span>
+      )}
+      <label htmlFor="bithDate" className="w-full p-2 border border-gray-300">
+        Date de naissance
+      </label>
       <input
         type="date"
         {...register("birthDate", { required: true })}
         placeholder="Date de naissance"
+        className="w-full p-2 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500"
       />
-      {errors.birthDate && <span>Ce champ est requis</span>}
+      {errors.birthDate && (
+        <span className="text-red-500 text-sm">Ce champ est requis</span>
+      )}
 
       <input
         {...register("birthPlace", { required: true })}
         placeholder="Lieu de naissance"
+        className="w-full p-2 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500"
       />
-      {errors.birthPlace && <span>Ce champ est requis</span>}
+      {errors.birthPlace && (
+        <span className="text-red-500 text-sm">Ce champ est requis</span>
+      )}
 
       <input
         {...register("nationality", { required: true })}
         placeholder="Nationalité"
+        className="w-full p-2 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500"
       />
-      {errors.nationality && <span>Ce champ est requis</span>}
+      {errors.nationality && (
+        <span className="text-red-500 text-sm">Ce champ est requis</span>
+      )}
 
       <input
         {...register("currentCity", { required: true })}
         placeholder="Ville actuelle"
+        className="w-full p-2 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500"
       />
-      {errors.currentCity && <span>Ce champ est requis</span>}
+      {errors.currentCity && (
+        <span className="text-red-500 text-sm">Ce champ est requis</span>
+      )}
 
       <input
         {...register("profession", { required: true })}
         placeholder="Profession"
+        className="w-full p-2 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500"
       />
-      {errors.profession && <span>Ce champ est requis</span>}
+      {errors.profession && (
+        <span className="text-red-500 text-sm">Ce champ est requis</span>
+      )}
 
-      <button type="submit">Envoyer</button>
+      <button
+        type="submit"
+        className="w-full py-2 px-4 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      >
+        Envoyer
+      </button>
     </form>
   );
 }
